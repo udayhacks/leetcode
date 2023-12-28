957ba72989efc5c3f7d1f43bcac5e2106226553d
uday <blackdevil190402@gmail.com>
2023-12-28 22:53:28 +0530

    added at 28/12/23

--- previousGreater.py
+++ previousGreater.py
@@ -1,2 +1,20 @@
+
+l = [60,20,19,10,40,30,9]
+
+
+
+
 def previousGreater(l):
-    sta
+    stack = []
+    print(-1)
+    stack.append(l[0])
+    
+    for i in range(1, len(l)) :
+        while len(stack) != 0 and stack[-1]< l[i] :
+            stack.pop()   
+        print(stack[-1])
+        stack.append(l[i])  
+        
+        
+previousGreater(l)   
+    
