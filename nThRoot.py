2d055d1fe2aac6a49df7a9070044bf68960b06a1
uday <blackdevil190402@gmail.com>
2024-07-03 22:53:22 +0530

    uday

--- nThRoot.py
+++ nThRoot.py
@@ -1,37 +1,43 @@
-def nThroot(n,m):
-    
-    def func(n,mid,m):
+
+
+def func(n,mid,m):
+        
         ans = 1
         for i in range(n):
-            ans *= mid
-            
+            ans *= mid  
             if ans > m :
                  return 2 
-             
-            if ans == m :
-                return 0 
+        if ans == m :
+            return 0 
         return 1 
+def nThroot(n,m):
+    
+
     
     
     l = 0 
     h = m 
-    while l< h :
+     
+    while l <= h :
         mid = (l+h)//2
         k = func(n,mid,m)
         if k == 0 :
             return mid 
         if k == 2 :
-            l = mid-1
+            h = mid-1
         if k == 1 :
-            h= m+1
+            l= mid+1
             
-    
     return -1 
 
+k = 64**(1/4)
+print(k)
 
+n = 3
+m = 3**3
 
-n = 4 
-m = 64
+print(nThroot(n,m))
+
 
 
 
