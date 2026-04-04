7c5b222259b954f803e16fd9f73a7fc4f7b55672
uday kanchanapally <71202015+udayhacks@users.noreply.github.com>
2026-04-04 13:10:53 +0530

    Create 3471-MinimumAverageOfSmallestAndLargestElements

---
+++ 3471-MinimumAverageOfSmallestAndLargestElements/3471-MinimumAverageOfSmallestAndLargestElements.py
@@ -0,0 +1,18 @@
+# Last updated: 04/04/2026, 13:10:53
+class Solution:
+    def minimumAverage(self, nums: List[int]) -> float:
+        
+        nums.sort()
+        i = 0 
+        h= sys.maxsize 
+        j = len(nums)-1
+        while i < j:
+            h = min((nums[i]+nums[j])/2,h)
+            i+=1
+            j-=1
+            
+        return h
+            
+            
+            
+        
